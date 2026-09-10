cfa976ee90b40092a73eb24569fc5e64d5fc2fd7
Bandi Mahesh Chandra Reddy <bunnyreddybandi222@gmail.com>
2026-09-10 09:49:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -179,4 +179,8 @@ app.get("/api/metrics", (req, res) => {
   return res.json(data);
 });
 
-app.listen(5000, () => console.log("✅ Backend running at port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Backend running at port ${PORT}`);
+});
