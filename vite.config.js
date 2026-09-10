a7641a9e497cc7aa05f534f25fb06b78c0a0b397
Bandi Mahesh Chandra Reddy <bunnyreddybandi222@gmail.com>
2026-09-10 10:16:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
   preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT,
-    allowedHosts: ['strivaira.onrender.com']
-  }
-})
+    host: "0.0.0.0",
+    port: Number(process.env.PORT) || 5173,
+    allowedHosts: ["strivaira.onrender.com"],
+  },
+});
